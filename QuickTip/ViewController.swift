f6ef6180fade41872abd749cc8716498d47e29f8
Cesa salaam <cesa.salaam@gmail.com>
2016-12-15 10:12:23 -0500

    Initial Commit

---
+++ QuickTip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickTip
+//
+//  Created by Cesa Salaam on 12/15/16.
+//  Copyright © 2016 Cesa Salaam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
